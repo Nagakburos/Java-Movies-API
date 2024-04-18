1ae2f666077b497976cbb0678ee2fed4cff957de
Nagakaburos <wansanice@gmail.com>
2024-04-18 11:03:03 -0300

    init

---
+++ src/main/java/com/br/nagakaburos/movies/Movie.java
@@ -0,0 +1,27 @@
+package com.br.nagakaburos.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbID;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<Review> reviewIds;
+}
