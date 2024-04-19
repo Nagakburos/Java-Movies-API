f431aee2af579fc9bec52acbb097f173a257db1c
Nagakaburos <wansanice@gmail.com>
2024-04-19 15:24:59 -0300

    updates

--- src/main/java/com/br/nagakaburos/movies/MovieController.java
+++ src/main/java/com/br/nagakaburos/movies/MovieController.java
@@ -1,10 +1,17 @@
 package com.br.nagakaburos.movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
-
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+    private MovieService movieService
+    @GetMapping
+    public ResponseEntity<String> getAllMovies() {
+        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    }
 }
