b897a8c2ef71aa567e4eb59ad31f14519029ae60
Nagakburos <wansanice@proton.me>
2024-04-25 17:17:10 -0300

    BACKEND finish

--- src/main/java/com/br/nagakaburos/movies/ReviewController.java
+++ src/main/java/com/br/nagakaburos/movies/ReviewController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     private ReviewService reviewService;
 
