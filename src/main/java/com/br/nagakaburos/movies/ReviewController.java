10549f9eba9b66849c65da2032cd06294aeae885
Nagakburos <wansanice@proton.me>
2024-04-25 16:46:03 -0300

    Review implements

---
+++ src/main/java/com/br/nagakaburos/movies/ReviewController.java
@@ -0,0 +1,4 @@
+package com.br.nagakaburos.movies;
+
+public class ReviewController {
+}
--- src/main/java/com/br/nagakaburos/movies/ReviewService.java
+++ src/main/java/com/br/nagakaburos/movies/ReviewService.java
@@ -14,14 +14,13 @@ public class ReviewService {
     @Autowired
     private MongoTemplate mongoTemplate;
     public  Review createReview (String reviewBody, String imdbId) {
-        Review review = new Review(reviewBody);
-
-        reviewRepository.insert(review);
+        Review review = reviewRepository.insert(new Review(reviewBody));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
-                .first(1,25);
+                .first();
 
+        return review;
     }
 }
