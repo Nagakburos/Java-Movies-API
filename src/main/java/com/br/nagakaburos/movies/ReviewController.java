38a6696ed5e3163b50f9f22a06c8ef649ee40015
Nagakburos <wansanice@proton.me>
2024-04-25 17:04:11 -0300

    Review Controller implements

--- src/main/java/com/br/nagakaburos/movies/ReviewController.java
+++ src/main/java/com/br/nagakaburos/movies/ReviewController.java
@@ -1,4 +1,21 @@
 package com.br.nagakaburos.movies;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class ReviewController {
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
 }
