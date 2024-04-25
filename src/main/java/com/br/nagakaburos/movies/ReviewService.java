6b2f1725e77004b1ef61557a145d543a7b153bf8
Nagakburos <wansanice@proton.me>
2024-04-25 11:12:32 -0300

    ReviewService implements

--- src/main/java/com/br/nagakaburos/movies/ReviewService.java
+++ src/main/java/com/br/nagakaburos/movies/ReviewService.java
@@ -1,4 +1,27 @@
 package com.br.nagakaburos.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public  Review createReview (String reviewBody, String imdbId) {
+        Review review = new Review(reviewBody);
+
+        reviewRepository.insert(review);
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first(1,25);
+
+    }
 }
